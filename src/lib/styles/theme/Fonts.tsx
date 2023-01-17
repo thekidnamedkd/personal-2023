7e40ea907b90eba3bc3918800244b33544200927
thekidnamedkd <whomst@kevind.xyz>
2023-01-17 20:08:22 +0100

    chore: fix font styles

--- src/lib/styles/theme/Fonts.tsx
+++ src/lib/styles/theme/Fonts.tsx
@@ -22,51 +22,6 @@ const Fonts = () => (
         font-weight: normal;
         font-style: normal;
       }
-      /* quote */
-      @font-face {
-        font-family: 'ITC Garamond';
-        font-style: normal;
-        font-weight: 300;
-        font-display: swap;
-        src: url('/fonts/ITCGaramondLightNarrow/font.woff2') format('woff2'), url('/fonts/ITCGaramondLightNarrow/font.woff') format('woff');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      /* body medium */
-      @font-face {
-        font-family: 'Neue Haas';
-        font-style: normal;
-        font-weight: 500;
-        font-display: swap;
-        src: url('/fonts/NeueHaas/Medium.woff') format('woff');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      /* body medium italic */
-      @font-face {
-        font-family: 'Neue Haas';
-        font-style: italic;
-        font-weight: 500;
-        font-display: swap;
-        src: url('/fonts/NeueHaas/MediumItalic.woff') format('woff');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      /* body regular */
-      @font-face {
-        font-family: 'Neue Haas';
-        font-style: normal;
-        font-weight: 400;
-        font-display: swap;
-        src: url('/fonts/NeueHaas/Roman.woff') format('woff');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      /* body regular italic */
-      @font-face {
-        font-family: 'Neue Haas';
-        font-style: italic;
-        font-weight: 400;
-        font-display: swap;
-        src: url('/fonts/NeueHaas/RomanItalic.woff') format('woff');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
       `}
   />
 );

fddf7f98f32acdb164bc7fa4d263d3033c43056a
thekidnamedkd <whomst@kevind.xyz>
2023-01-18 22:13:41 +0100

    chore: remove box shadow on navs

--- src/lib/layout/Navigation.tsx
+++ src/lib/layout/Navigation.tsx
@@ -14,7 +14,6 @@ const Navigation = () => {
     >
       <Button
         onClick={() => navigate("/")}
-        boxShadow={location.pathname === "/" ? "inset 0 0 2px #333333" : "none"}
         border={
           location.pathname === "/" ? "2px solid black" : "1px solid black"
         }
@@ -23,9 +22,6 @@ const Navigation = () => {
       </Button>
       <Button
         onClick={() => navigate("/reading")}
-        boxShadow={
-          location.pathname === "/reading" ? "inset 0 0 2px #333333" : "none"
-        }
         border={
           location.pathname === "/reading"
             ? "2px solid black"
